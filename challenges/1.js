b7a06f8c58cc7aeb064e0e6d4bfebd51d11dee38
Kaushik Chemburkar <attherate@gmail.com>
2022-03-23 17:06:40 +1100

    changed prettier settings - double quotation marks

--- challenges/1.js
+++ challenges/1.js
@@ -1,5 +1,5 @@
-const firstName = 'Kaushik'
-const lastName = 'Chemburkar'
-var address = ' main street '
-address = 'first street'
+const firstName = "Kaushik"
+const lastName = "Chemburkar"
+var address = " main street "
+address = "first street"
 console.log(firstName, lastName, address)
