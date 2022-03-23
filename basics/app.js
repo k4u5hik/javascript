1ba4ad8ba02891f0b3e580b66e83a571a3a84f8d
Kaushik Chemburkar <attherate@gmail.com>
2022-03-24 09:33:51 +1100

    concat

--- basics/app.js
+++ basics/app.js
@@ -1,1 +1,4 @@
+// const name = "John"
+// const age = 30
 
+// console.log("My name is " + name + " and I am " + age + " years old.")
---
+++ challenges/2.js
@@ -0,0 +1,5 @@
+const street = "4 Privet Drive"
+const country = "Little Whinging"
+const city = "England"
+let fullAddress = `${street}, ${country}, ${city}`
+console.log(fullAddress)
