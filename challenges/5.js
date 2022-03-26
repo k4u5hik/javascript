a8404d4e484ff4cafca9b75132c9c84117352c67
Kaushik Chemburkar <attherate@gmail.com>
2022-03-26 20:47:05 +1100

    array challenge

--- challenges/5.js
+++ challenges/5.js
@@ -1,3 +1,9 @@
-const friends = ["Kaushik", "Madhura", "Yuki", "Barbaloot", "Mooshoo"]
+// const friends = ["Kaushik", "Madhura", "Yuki", "Barbaloot", "Mooshoo"]
 
-console.log(friends[1])
+// console.log(friends[1])
+
+var fruits = ["apple", "banana", "orange", "pineapple", "strawberry", "25"]
+var myfruit = fruits[1]
+fruits[5] = "pineapple"
+console.log(myfruit)
+console.log(fruits)
