2336293ff1cca1b1abd85fcf8cc7040ad7b15b90
Kaushik Chemburkar <attherate@gmail.com>
2022-03-27 19:57:36 +1100

    Difference between params and args in comment

--- challenges/7-paramsAndArgs.js
+++ challenges/7-paramsAndArgs.js
@@ -8,3 +8,4 @@ hello("Joe")
 // Function parameters are the names listed in the function's definition.
 // Function arguments are the real values passed to the function.
 // Parameters are initialized to the values of the arguments supplied.
+// In the above example, the parameters is "name" and the argument is "John".
