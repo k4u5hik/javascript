92a5f2467db11c8b035c8e659618d089684ffdbd
Kaushik Chemburkar <attherate@gmail.com>
2022-03-28 09:58:46 +1100

    How a function works! - rearranged comments -

--- challenges/7-paramsAndArgs.js
+++ challenges/7-paramsAndArgs.js
@@ -1,23 +1,23 @@
-// Function parameters are the names listed in the function's definition.
-// Function arguments are the real values passed to the function.
-// Parameters are initialized to the values of the arguments supplied.
-// In the above example, the parameters is "name" and the argument is "John".
-
-// 1inch is equal to 2.54cm
+// 1 inch is equal to 2.54cm
 
 function convertToCm(inches) {
+  // Function parameters are the names listed in the function's definition.
+  // The function is called with the parameters initialized to the arguments.
   const newValue = inches * 2.54
   return newValue
+  // return is the value that is returned from the function.
 }
 const width = convertToCm(1)
+// Function arguments are the real values passed to the function.
+// The function is called with the arguments supplied.
+// Parameters are initialized to the values of the arguments supplied.
 const height = convertToCm(2)
+// Function arguments are the real values passed to the function.
+// The function is called with the arguments supplied.
+// Parameters are initialized to the values of the arguments supplied.
 
 const dimensionInCm = [width, height]
 console.log(dimensionInCm)
 
 // Output
 // [2.54, 5.08]
-
-// return is the value that is returned from the function.
-// The function is called with the arguments supplied.
-// The function is called with the parameters initialized to the arguments.
