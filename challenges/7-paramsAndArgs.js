ba15d915c6eac7cb76feeac1166a29ecddbc581a
Kaushik Chemburkar <attherate@gmail.com>
2022-03-27 19:55:47 +1100

    passing args in a function

---
+++ challenges/7-paramsAndArgs.js
@@ -0,0 +1,10 @@
+function hello(name) {
+  console.log(`Hello, ${name}!`)
+}
+hello("John")
+hello("Mary")
+hello("Joe")
+
+// Function parameters are the names listed in the function's definition.
+// Function arguments are the real values passed to the function.
+// Parameters are initialized to the values of the arguments supplied.
