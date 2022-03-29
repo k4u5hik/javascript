3c407d90b440574a425c55106a7e01e95411d60b
Kaushik Chemburkar <attherate@gmail.com>
2022-03-29 23:06:36 +1100

    switch statement

---
+++ challenges/14-switch.js
@@ -0,0 +1,38 @@
+// switch statement
+
+// const dice = 0
+
+// if (dice === 1) {
+//   console.log("You rolled a 1!")
+// }
+// if (dice === 2) {
+//   console.log("You rolled a 2!")
+// }
+// if (dice < 1 || dice > 6) {
+//   console.log("You did not roll the dice!")
+// }
+
+const dice = 1
+
+switch (dice) {
+  case 1:
+    console.log("You rolled a 1!")
+    break
+  case 2:
+    console.log("You rolled a 2!")
+    break
+  case 3:
+    console.log("You rolled a 3!")
+    break
+  case 4:
+    console.log("You rolled a 4!")
+    break
+  case 5:
+    console.log("You rolled a 5!")
+    break
+  case 6:
+    console.log("You rolled a 6!")
+    break
+  default:
+    console.log("You did not roll the dice!")
+}
