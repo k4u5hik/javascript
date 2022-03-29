7d740ed7c6912c5a2b7e73f50554c5f7989a83cb
Kaushik Chemburkar <attherate@gmail.com>
2022-03-29 11:41:28 +1100

    equality operators

---
+++ challenges/12-equality.js
@@ -0,0 +1,10 @@
+const num1 = 6
+const num2 = "6"
+
+const equality1 = num1 == num2
+const equality2 = num1 === num2
+
+console.log(equality1, equality2)
+
+// Double equal sign compares the value of the variables
+// Triple equal sign compares the value and the type of the variables
