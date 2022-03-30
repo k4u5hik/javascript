11edbdec3e6bd7cf7fc90fe98d13f27921004d69
Kaushik Chemburkar <attherate@gmail.com>
2022-03-31 08:30:07 +1100

    doWhile

---
+++ challenges/17-doWhile.js
@@ -0,0 +1,10 @@
+// do while loop will run at least once
+// runs a loop while condition is true
+// code block first, condition second
+
+let amount = 10
+
+do {
+  console.log(amount)
+  amount--
+} while (amount > 0)
