51731d7fcde76991cae3a4846d36c8942d506042
Kaushik Chemburkar <attherate@gmail.com>
2022-03-31 14:55:40 +1100

    Passing an object as a parameter

---
+++ challenges/19-stringsChallenge.js
@@ -0,0 +1,13 @@
+// function fullName(firstName, lastName) {
+//   const fullName = `${firstName} ${lastName}`
+//   return fullName.toUpperCase()
+// }
+// const actualName = fullName("John", "Doe")
+// console.log(actualName)
+
+function fullName({ firstName, lastName }) {
+  const fullName = `${firstName} ${lastName}`
+  return fullName.toUpperCase()
+}
+
+console.log(fullName({ firstName: "John", lastName: "Doe" })));
