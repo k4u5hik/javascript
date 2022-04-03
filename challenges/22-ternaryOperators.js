08b04f0af6e72ffe0e389d0746e266194d2f8543
Kaushik Chemburkar <attherate@gmail.com>
2022-04-03 14:48:40 +1000

    Conditional (ternary) operators

---
+++ challenges/22-ternaryOperators.js
@@ -0,0 +1,8 @@
+// Conditional (ternary) operators are a way to write if statements in a more compact way
+// unary operators are used to change the value of a variable
+// examples of unary operators are ++, --, !, typeof, delete
+// binary operators are used to compare two values
+// examples of binary operators are ==, ===, !=, !==, >, <, >=, <=, +, -, *, /, %
+
+const value = 1 < 2 ? "yes" : "no"
+console.log(value)
