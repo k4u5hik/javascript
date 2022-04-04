b0e90c5419a192c0d80d6f3e44cd35e0e1e29e71
Kaushik Chemburkar <attherate@gmail.com>
2022-04-04 15:11:52 +1000

    map

--- challenges/24-forEach.js
+++ challenges/24-forEach.js
@@ -9,6 +9,7 @@
 // The forEach method returns undefined.
 // The following code logs the numbers 1 through 5 to the console.
 // The forEach method is called on the array [1, 2, 3, 4, 5].
+// does not return a new array.
 
 const people = [
   { name: "John", age: 20, job: "developer" },
@@ -16,8 +17,18 @@ const people = [
   { name: "Joe", age: 22, job: "teacher" },
 ]
 
-function showPerson(person) {
-  console.log(person.job.toUpperCase())
-}
+// function showPerson(person) {
+//   console.log(person.job.toUpperCase())
+// }
 
-people.forEach(showPerson)
+// people.forEach(showPerson)
+
+// map does return a new array.
+// map does not change size of the original array.
+// map uses values from the original array as arguments to the callback function.
+
+console.log(map1)
+// expected output: Array [2, 8, 18, 32]
+
+const ages = people.map((person) => person.age)
+console.log(ages)
