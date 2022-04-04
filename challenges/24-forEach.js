a7f54f11c62ff6a99a42ef447575b5c44d0c5b42
Kaushik Chemburkar <attherate@gmail.com>
2022-04-04 14:58:56 +1000

    forEach

---
+++ challenges/24-forEach.js
@@ -0,0 +1,23 @@
+// forEach is a method that takes a callback function as an argument.
+// The callback function is called for each element in the array.
+// The callback function is called with three arguments:
+// 1. The current element being processed in the array.
+// 2. The index of the current element being processed in the array.
+// 3. The array itself.
+// The callback function must return a value.
+// The return value is ignored.
+// The forEach method returns undefined.
+// The following code logs the numbers 1 through 5 to the console.
+// The forEach method is called on the array [1, 2, 3, 4, 5].
+
+const people = [
+  { name: "John", age: 20, job: "developer" },
+  { name: "Jane", age: 21, job: "designer" },
+  { name: "Joe", age: 22, job: "teacher" },
+]
+
+function showPerson(person) {
+  console.log(person.job)
+}
+
+people.forEach(showPerson)
