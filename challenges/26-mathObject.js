e89c1f5a251d02be8dc230c4ca9265f3a0adf088
Kaushik Chemburkar <attherate@gmail.com>
2022-04-05 20:00:07 +1000

    Math Object

---
+++ challenges/26-mathObject.js
@@ -0,0 +1,17 @@
+// Rounding numbers
+
+// const number = 4.124423
+// const result = Math.round(number)
+// console.log("🚀 ~ file: 26-mathObject.js ~ line 3 ~ result", result)
+
+// const number = 4.124423
+// const result = Math.ceil(number)
+// console.log("🚀 ~ file: 26-mathObject.js ~ line 9 ~ result", result)
+
+// const number = 4.124423
+// const result = Math.round(number)
+// console.log("🚀 ~ file: 26-mathObject.js ~ line 13 ~ result", result)
+
+const number = 4.124423
+const result = Math.sqrt(number)
+console.log("🚀 ~ file: 26-mathObject.js ~ line 17 ~ result", result)
