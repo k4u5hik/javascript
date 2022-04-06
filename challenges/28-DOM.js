8270bacda1fcd3642380b3090778268e1472ae92
Kaushik Chemburkar <attherate@gmail.com>
2022-04-06 12:36:28 +1000

    Basic DOM for making CSS changes

---
+++ challenges/28-DOM.js
@@ -0,0 +1,16 @@
+// DOM is a programming interface for HTML and XML documents that provides a way to access and manipulate the content of the document.
+// DOM is a tree structure that represents the content of an HTML or XML document.
+// Select element or a group of elements that you want to apply an action to.
+
+// console.log(window)
+// console.dir(document)
+// console.log(document)
+
+// document is a property of the window object
+
+// console. log() returns the object in its string representation and console. dir() recognizes the object just as an object and outputs its properties.
+
+// https://stackoverflow.com/questions/11954152/whats-the-difference-between-console-dir-and-console-log
+
+const h1 = document.getElementById("title")
+h1.style.color = "red"
