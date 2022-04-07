6cdf723bc6192725b10821afd7e4e6270f755abb
Kaushik Chemburkar <attherate@gmail.com>
2022-04-07 13:23:14 +1000

    textContent nodeValue IMP - getting the text content inside the element displayed in the console log

---
+++ challenges/31-textContentnodeValue.js
@@ -0,0 +1,9 @@
+// nodeValue
+// textContent
+
+const item = document.getElementById("special");
+const value = item.firstChild.nodeValue;
+const easyValue = item.textContent;
+
+console.log(value);
+console.log(easyValue);
