7abac4867ef605f0eae6ba73d30b7e78734c1cb3
Kaushik Chemburkar <attherate@gmail.com>
2022-04-10 17:39:19 +1000

    stop propagation

--- challenges/36-bubblingCapturing.js
+++ challenges/36-bubblingCapturing.js
@@ -1,8 +1,8 @@
 // allows selecting dynamic elements
-// event propogation - order the events are fired
+// event propagation - order the events are fired
 // bubbling - event bubbles up the DOM tree
 // capturing - event fires from the bottom up
-// event bubbling - clicked element first, then parent, then grandparent, etc., (bubbles up)
+// event bubbling - clicked element first, then parent, then grandparent, etc., (bubbles up) - default
 // event capturing - fires at the root and fires until it reaches the clicked element (captures)/target
 
 const container = document.querySelector(".container");
@@ -12,11 +12,16 @@ function showBubbling(e) {
   console.log("current target", e.currentTarget);
   console.log("bubbling", e.target);
   if (e.target.classList.contains("link")) {
-    e.target.classList.toggle("active");
+    console.log("you clicked a link");
   }
 }
 
-list.addEventListener("click", showBubbling);
-// container.addEventListener("click", showBubbling);
-// document.addEventListener("click", showBubbling);
-// window.addEventListener("click", showBubbling);
+function stopPropagation(e) {
+  console.log("You clicked on a list (stopped propagation)");
+  e.stopPropagation();
+}
+
+list.addEventListener("click", stopPropagation);
+container.addEventListener("click", showBubbling);
+document.addEventListener("click", showBubbling);
+window.addEventListener("click", showBubbling);
