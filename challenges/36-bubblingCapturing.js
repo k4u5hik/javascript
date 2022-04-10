d11cb2c921c961668e1cb5dbd5dd15f8e04574ff
Kaushik Chemburkar <attherate@gmail.com>
2022-04-10 17:31:26 +1000

    bubbling basics

---
+++ challenges/36-bubblingCapturing.js
@@ -0,0 +1,19 @@
+// allows selecting dynamic elements
+// event propogation - order the events are fired
+// bubbling - event bubbles up the DOM tree
+// capturing - event fires from the bottom up
+// event bubbling - clicked element first, then parent, then grandparent, etc., (bubbles up)
+// event capturing - fires at the root and fires until it reaches the clicked element (captures)/target
+
+const container = document.querySelector(".container");
+const list = document.querySelector(".list-items");
+
+function showBubbling(e) {
+  console.log("current target", e.currentTarget);
+  console.log("bubbling", e.target);
+}
+
+list.addEventListener("click", showBubbling);
+container.addEventListener("click", showBubbling);
+document.addEventListener("click", showBubbling);
+window.addEventListener("click", showBubbling);
