fe8bd63ebbf968b19e02940a687bc09bc4e8f1c4
Kaushik Chemburkar <attherate@gmail.com>
2022-04-10 17:32:55 +1000

    toggle target active onClick

--- challenges/36-bubblingCapturing.js
+++ challenges/36-bubblingCapturing.js
@@ -11,9 +11,12 @@ const list = document.querySelector(".list-items");
 function showBubbling(e) {
   console.log("current target", e.currentTarget);
   console.log("bubbling", e.target);
+  if (e.target.classList.contains("link")) {
+    e.target.classList.toggle("active");
+  }
 }
 
 list.addEventListener("click", showBubbling);
-container.addEventListener("click", showBubbling);
-document.addEventListener("click", showBubbling);
-window.addEventListener("click", showBubbling);
+// container.addEventListener("click", showBubbling);
+// document.addEventListener("click", showBubbling);
+// window.addEventListener("click", showBubbling);
