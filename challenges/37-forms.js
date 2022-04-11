0e17c65c2a74236ab9fcc5a7b09ad3317711de4b
Kaushik Chemburkar <attherate@gmail.com>
2022-04-11 16:11:20 +1000

    basic form submit

---
+++ challenges/37-forms.js
@@ -0,0 +1,10 @@
+const form = document.getElementById("form");
+const name = document.getElementById("name");
+const password = document.getElementById("password");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log("form submitted");
+  console.log(name.value);
+  console.log(password.value);
+});
