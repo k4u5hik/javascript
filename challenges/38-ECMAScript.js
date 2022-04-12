894e62decf16bb13ac93675d27bd6cfeb611e217
Kaushik Chemburkar <attherate@gmail.com>
2022-04-12 11:20:11 +1000

    ECMAScript basics

---
+++ challenges/38-ECMAScript.js
@@ -0,0 +1,8 @@
+// Define, Update and Redefine:
+// - You can define, update and redefine VAR
+// - You can define and update LET but cannot redefine it
+// - You can define CONST but cannot update or redefine it
+
+// Scope:
+// - LET & CONST are block scoped - anything within {} - same nesting level only
+// - VAR is (so can LET & CONST be) function scoped - can be accessible within that function and cannot be used outside that function
