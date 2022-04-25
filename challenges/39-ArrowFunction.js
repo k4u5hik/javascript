15c23a97e7c14525b392a6e8e46d64e7a5ae0780
Kaushik Chemburkar <attherate@gmail.com>
2022-04-26 09:56:14 +1000

    Arrow function basics

---
+++ challenges/39-ArrowFunction.js
@@ -0,0 +1,13 @@
+const sayHi = (name) => {
+  console.log(`Hello ${name}`);
+};
+sayHi("Kaushik");
+
+const double = (value) => value * 2;
+const num = double(2);
+console.log(num);
+
+// two parameters
+const multiply = (a, b) => a * b;
+const result = multiply(2, 3);
+console.log(result);
