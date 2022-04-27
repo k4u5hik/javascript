00f19ad392c3c4d4d3e54f0fb261510c39b20700
Kaushik Chemburkar <attherate@gmail.com>
2022-04-27 14:58:51 +1000

    onClick change element to black then red in 1sec - used arrow function for global scope

--- challenges/40-arrowThis.js
+++ challenges/40-arrowThis.js
@@ -8,4 +8,7 @@ const btn = document.querySelector(".btn");
 btn.addEventListener("click", function () {
   console.log(this);
   this.style.color = "black";
+  setTimeout(() => {
+    this.style.color = "red";
+  }, 1000);
 });
