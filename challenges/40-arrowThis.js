8ec65a0179bde25a1fb45503f2e88fd94c9cee24
Kaushik Chemburkar <attherate@gmail.com>
2022-04-27 14:54:33 +1000

    callback, click event, change element color

--- challenges/40-arrowThis.js
+++ challenges/40-arrowThis.js
@@ -2,44 +2,10 @@
 // regular function: this refers to the left of the dot
 // arrow function: this refers to it's current surrounding scope
 
-const bob = {
-  firstName: "Bob",
-  lastName: "Jones",
-  phoneNumber: "(650) 777-7777",
-  email: "bob.jones@gmail.com",
-  children: ["Susie", "Mike", "Joe"],
-  address: {
-    street: "111 Main St.",
-    city: "San Francisco",
-    state: "CA",
-    zip: 94111,
-  },
-  sayName: function () {
-    console.log(this);
-    setTimeout(() => {
-      //   console.log(this);
-      console.log(`${this.firstName} ${this.lastName}`);
-    }, 2000);
-  },
-};
+// select elements with this
 
-const anna = {
-  firstName: "Anna",
-  lastName: "Smith",
-  phoneNumber: "(650) 888-8888",
-  email: "annasmith@gmail.com",
-  children: ["Mary", "Shelley"],
-  address: {
-    street: "222 Main St.",
-    city: "San Francisco",
-    state: "CA",
-    zip: 94111,
-  },
-  sayName: () => {
-    console.log(this);
-    console.log(`${this.firstName} ${this.lastName}`);
-  },
-};
-
-bob.sayName();
-// anna.sayName();
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", function () {
+  console.log(this);
+  this.style.color = "black";
+});
