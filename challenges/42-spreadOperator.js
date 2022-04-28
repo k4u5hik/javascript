16612ea23fc838fedd884910aff70baf0a116e65
Kaushik Chemburkar <attherate@gmail.com>
2022-04-28 14:32:26 +1000

    spread as function args

--- challenges/42-spreadOperator.js
+++ challenges/42-spreadOperator.js
@@ -1,8 +1,12 @@
-const headings = document.querySelectorAll("h1");
-const result = document.getElementById("result");
+const numbers = [1, 2, 3, 4, 5];
+console.log(Math.max(...numbers));
 
-const text = [...headings]
-  .map((item) => `<span> ${item.textContent} </span>`)
-  .join(" ");
+// spread operator is used to pass an array as arguments to a function
 
-result.innerHTML = text;
+const john = ["John", "Smith", 1990, "designer", false];
+
+const sayHello = (firstName, lastName, dob) => {
+  console.log(firstName + " " + lastName + " " + dob);
+};
+
+sayHello(...john);
