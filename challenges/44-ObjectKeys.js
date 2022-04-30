d6b3d26ee475a2d5cda4cef4179a6b3788e210c9
Kaushik Chemburkar <attherate@gmail.com>
2022-04-30 16:44:56 +1000

    Object.Values

--- challenges/44-ObjectKeys.js
+++ challenges/44-ObjectKeys.js
@@ -6,3 +6,12 @@ const object1 = {
 
 console.log(Object.keys(object1));
 // expected output: Array ["a", "b", "c"]
+
+const object2 = {
+  a: "somestring",
+  b: 42,
+  c: false,
+};
+
+console.log(Object.values(object2));
+// expected output: Array ["somestring", 42, false]
