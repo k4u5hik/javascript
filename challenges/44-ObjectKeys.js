26570b8220d42f93f4af59c8dcd4881c1f66a14f
Kaushik Chemburkar <attherate@gmail.com>
2022-04-30 17:00:12 +1000

    Object.entries() -The Object.entries() method returns an array of a given object's own enumerable string-keyed property [key, value] pairs.

--- challenges/44-ObjectKeys.js
+++ challenges/44-ObjectKeys.js
@@ -1,3 +1,5 @@
+// Object.keys()
+
 const object1 = {
   a: "somestring",
   b: 42,
@@ -7,6 +9,7 @@ const object1 = {
 console.log(Object.keys(object1));
 // expected output: Array ["a", "b", "c"]
 
+// Object.values()
 const object2 = {
   a: "somestring",
   b: 42,
@@ -15,3 +18,13 @@ const object2 = {
 
 console.log(Object.values(object2));
 // expected output: Array ["somestring", 42, false]
+
+// Object.entries()
+const object3 = {
+  a: "somestring",
+  b: 42,
+  c: false,
+};
+for (const [key, value] of Object.entries(object3)) {
+  console.log(`${key}: ${value}`);
+}
