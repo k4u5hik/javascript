5fb1536f049cdfec6e35496978727f44bc96b1df
Kaushik Chemburkar <attherate@gmail.com>
2022-05-02 14:01:50 +1000

    String.prototype.includes() - The includes() method performs a case-sensitive search to determine whether one string may be found within another string, returning true or false as appropriate. - includes() method is case sensitive

---
+++ challenges/46-StringPrototypeIncludes.js
@@ -0,0 +1,19 @@
+const sentence = "The quick brown fox jumps over the lazy dog.";
+
+const word = "fox";
+
+console.log(
+  `The word "${word}" ${
+    sentence.includes(word) ? "is" : "is not"
+  } in the sentence`
+);
+// expected output: "The word "fox" is in the sentence"
+
+const str = "To be, or not to be, that is the question.";
+
+console.log(str.includes("To be")); // true
+console.log(str.includes("question")); // true
+console.log(str.includes("nonexistent")); // false
+console.log(str.includes("To be", 1)); // false
+console.log(str.includes("TO BE")); // false
+console.log(str.includes("")); // true
