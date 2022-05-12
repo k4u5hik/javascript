7bcb2d374cba810667cf091506a2a510c73703b4
Kaushik Chemburkar <attherate@gmail.com>
2022-05-13 09:39:24 +1000

    Return value - Garbage Collection - the return value is not saved after the function has been run, this is why myTotal requires the function to be run after assignment to store the value in the const.

---
+++ beginner-javascript-master/practice/functions.js
@@ -0,0 +1,13 @@
+
+    // const p = document.querySelector('#para1');
+    // const c = document.querySelector('.classy');
+    // console.log(p)
+    // console.log(c)
+
+function result(price,qty) {
+    const total = price*qty
+    return total
+}
+
+const myTotal = result(25,2)
+console.log(myTotal);
