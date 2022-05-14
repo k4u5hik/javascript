6978411adaf0e60b8a7bc69e4c85c5f6e44c95d9
Kaushik Chemburkar <attherate@gmail.com>
2022-05-14 19:54:26 +1000

    Callback function onClick eventlistener

--- beginner-javascript-master/practice/functions.js
+++ beginner-javascript-master/practice/functions.js
@@ -4,14 +4,29 @@
     // console.log(p)
     // console.log(c)
 
-function result(price,qty) {
-    const total = price*qty
-    return total
+// function result(price,qty) {
+//     const total = price*qty
+//     return total
+// }
+
+// // const myTotal = result(25,2)
+// console.log(result(50,5));
+
+
+// const total = (a,b=3) => a+b
+// console.log(total(5));
+
+// const makeABaby = (name,age) => {
+//     return `${name} is a ${age} year old baby.`
+// }
+// console.log(makeABaby("Mooshoo",5));
+
+// Callback functions
+// Click Callback
+
+const button = document.querySelector('.clickme');
+
+function handleClick() {
+    console.log('Clicked');
 }
-
-// const myTotal = result(25,2)
-console.log(result(50,5));
-
-
-const total = (a,b=3) => a+b
-console.log(total(5));
+button.addEventListener('click',handleClick);
