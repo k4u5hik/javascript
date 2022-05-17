5356ce770bf26dcbd719e943094e2dfea0b0c6b2
Kaushik Chemburkar <attherate@gmail.com>
2022-05-18 09:21:41 +1000

    ADDING text - insertAdjacentText(where, data) - avoids re-rendering of the element

--- beginner-javascript-master/exercises/20 - The DOM/the-dom.js
+++ beginner-javascript-master/exercises/20 - The DOM/the-dom.js
@@ -10,9 +10,14 @@ const item2 = document.querySelector('.item2')
 const item2Image = item2.querySelector('img')
 const heading = document.querySelector('h2')
 heading.textContent = "Kaushik's new heading"
-console.dir(heading.textContent)
+// console.dir(heading.textContent)
 
-console.log(heading.innerText) // Aware of CSS styling
-console.log(heading.innerHTML)
-console.log(heading.outerHTML)
-console.log(heading.textContent)
+// console.log(heading.innerText) // Aware of CSS styling
+// console.log(heading.innerHTML)
+// console.log(heading.outerHTML)
+// console.log(heading.textContent)
+
+const pizzaList = document.querySelector('.pizza')
+console.log(pizzaList.textContent)
+console.log(pizzaList.outerHTML)
+pizzaList.insertAdjacentText("beforeend", ` Pizza is good! 🍕`);
