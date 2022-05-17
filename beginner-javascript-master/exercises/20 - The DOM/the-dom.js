41984f26fb74c2179b57cb609cc503a773313fdd
Kaushik Chemburkar <attherate@gmail.com>
2022-05-17 19:35:31 +1000

    Selecting DOM Elements

--- beginner-javascript-master/exercises/20 - The DOM/the-dom.js
+++ beginner-javascript-master/exercises/20 - The DOM/the-dom.js
@@ -4,3 +4,9 @@ function init(){
 }
 
 document.addEventListener('DOMContentLoaded', init)
+
+const imgs = document.querySelectorAll('.item img')
+const item2 = document.querySelector('.item2')
+const item2Image = item2.querySelector('img')
+const heading = document.querySelector('h2')
+console.dir(heading.textContent)
