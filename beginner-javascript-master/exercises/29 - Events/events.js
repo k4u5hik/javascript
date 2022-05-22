60aaedc973004f2fdb289eb095370be9ae904ba4
Kaushik Chemburkar <attherate@gmail.com>
2022-05-23 09:10:55 +1000

    query selector all + comment fix

--- beginner-javascript-master/exercises/29 - Events/events.js
+++ beginner-javascript-master/exercises/29 - Events/events.js
@@ -3,3 +3,8 @@ const butts = document.querySelector(".butts");
 butts.addEventListener("click", function () {
   console.log("Button Clicked!");
 });
+
+// listen on multiple items
+
+const buyButtons = document.querySelectorAll("button.buy");
+console.log(buyButtons);
