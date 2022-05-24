fac7c75f4d1572755a5155bca33d40e46229f561
Kaushik Chemburkar <attherate@gmail.com>
2022-05-24 19:06:33 +1000

    Photo button

--- beginner-javascript-master/exercises/29 - Events/forms.js
+++ beginner-javascript-master/exercises/29 - Events/forms.js
@@ -1,1 +1,9 @@
 console.log('it works')
+
+const b1 = document.querySelector('.button1')
+
+b1.addEventListener('click', function(e) {
+    e.preventDefault()
+    console.log('button 1 clicked')
+    }
+)
