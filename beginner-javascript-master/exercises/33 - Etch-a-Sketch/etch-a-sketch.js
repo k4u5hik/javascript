abb93d6dd44a08404744eba8b850e9130250f1bd
Kaushik Chemburkar <attherate@gmail.com>
2022-05-24 19:27:53 +1000

    Etch-A-Sketch - Prep - Divided the game into smaller tasks

---
+++ beginner-javascript-master/exercises/33 - Etch-a-Sketch/etch-a-sketch.js
@@ -0,0 +1,11 @@
+// select the elements on the page - canvas, shake button
+
+// setup our canvas for drawing
+
+// write a draw function
+
+// write a handler for the MediaKeys
+
+// clear/shake function
+
+// listen for arrow keys
