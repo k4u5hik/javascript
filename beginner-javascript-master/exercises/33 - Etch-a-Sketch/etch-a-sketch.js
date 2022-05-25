232a06635e4f2d304f2ef121818c7505f77b59b7
Kaushik Chemburkar <attherate@gmail.com>
2022-05-25 14:43:01 +1000

    basic etch-a-sketch completed

--- beginner-javascript-master/exercises/33 - Etch-a-Sketch/etch-a-sketch.js
+++ beginner-javascript-master/exercises/33 - Etch-a-Sketch/etch-a-sketch.js
@@ -3,6 +3,7 @@
 const canvas = document.querySelector('#etch-a-sketch');
 const ctx = canvas.getContext('2d');
 const shakeButton = document.querySelector(".shake");
+const MOVE_AMOUNT = 10;
 
 // setup our canvas for drawing
 // make a variable called height and width from the same properties on our canvas
@@ -24,13 +25,62 @@ ctx.stroke(); // draw it
 
 // write a draw function
 
+function draw({key}){
+  console.log(key);
+  //start a new path
+  ctx.beginPath();
+  //move to the last point
+  ctx.moveTo(x, y);
+  //draw a line to the new point
+    switch(key){
+        case 'ArrowUp':
+            y -= MOVE_AMOUNT;
+            break;
+        case 'ArrowDown':
+            y += MOVE_AMOUNT;
+            break;
+        case 'ArrowLeft':
+            x -= MOVE_AMOUNT;
+            break;
+        case 'ArrowRight':
+            x += MOVE_AMOUNT;
+            break;
+        default:
+            break;
+    }
+    ctx.lineTo(x, y);
+    ctx.stroke();
+}
+
 
 
 // write a handler for the MediaKeys
 
+
 function handleKey(e){
-    e.preventDefault();
-    console.log('handleKey');
+    if (e.key.includes('Arrow')){
+        e.preventDefault();
+        draw({key: e.key});
+        // console.log(e.key);
+        // switch(e.key){
+        //     case 'ArrowUp':
+        //         y -= 10;
+        //         break;
+        //     case 'ArrowDown':
+        //         y += 10;
+        //         break;
+        //     case 'ArrowLeft':
+        //         x -= 10;
+        //         break;
+        //     case 'ArrowRight':
+        //         x += 10;
+        //         break;
+        // }
+        // ctx.beginPath();
+        // ctx.moveTo(x, y);
+        // ctx.lineTo(x, y);
+        // ctx.stroke();
+    }
 }
 
 // clear/shake function
